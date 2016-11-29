dfc6f81b137a15ada804d294940c65fa13259ca2
Bhairav Mehta <bhairavmehta95@gmail.com>
2016-11-29 00:37:14 -0500

    Initial

---
+++ ask/__init__.py
@@ -0,0 +1,10 @@
+from . import alexa_io
+
+''' 
+Setting up some nice abstractions around good object oritented code.
+'''
+
+ResponseBuilder = alexa_io.ResponseBuilder
+alexa = alexa_io.VoiceHandler()
+Request = alexa_io.Request
+Response = alexa_io.Response
