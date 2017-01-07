3287d2b9548c894c6cfe37422920427cd8bbfa67
Bhairav <bhairavmehta95@github.com>
2017-01-06 23:46:38 -0500

    second submission finalization

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-SQLALCHEMY_DATABASE_URI = 'ENTER URI HERE'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bhairavm:Soccerfam95!@flashcard-helper-alexa-skill-db.cvo93xzlcjwx.us-east-1.rds.amazonaws.com:3306/flashcard_helper'
